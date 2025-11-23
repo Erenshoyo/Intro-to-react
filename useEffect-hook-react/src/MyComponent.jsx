b5d82007385775ec10d6ddf9ebe282db3c4b2d6f
Md. Asif Shahriar Tauhid <70537695+Erenshoyo@users.noreply.github.com>
2025-11-23 12:06:59 +0600

    Use effect hook react

---
+++ useEffect-hook-react/src/MyComponent.jsx
@@ -0,0 +1,84 @@
+//useEffect() = React hook that tells React - "Do some code when -
+//                                             *This component rerenders
+//                                             *This component mounts
+//                                             *the state of a value"
+
+// useEffect(function, [dependencies])
+
+// 1. useEffect(() => {})               //Runs after every re-render.
+// 2. useEffect(() => {}, [])           //Runs only on mount.
+// 3. useEffect(() => {}, [value])      // Runs on mount + when value changes
+
+// USES
+// #1 Event  Listeners
+// #2 DOM Manipulation
+// #3 Subscriptions (real-time updates)
+// #4 Fetching Data from an API
+// #5 Clean up when a component unmounts
+
+import React, { useState, useEffect } from "react";
+
+function MyComponent() {
+  // const[count, setCount] = useState(0);
+  // const[color, setColor] = useState("green");
+
+  // useEffect(() => {
+  //     document.title = `Count: ${count} ${color}`;
+
+  //     return() =>{
+  //         //Some cleanup code
+  //     }
+  // },[count, color]);
+
+  // function addCount(){
+  //     setCount(c => c + 1);
+  // }
+  // function subtractCount(){
+  //     setCount(c => c - 1);
+  // }
+  // function changeColor(){
+  //     setColor(c => c === "green" ? "red" : "green")
+  // }
+  // return(
+  //     <>
+  //         <p style={
+  //             {color:color}
+  //         }>Count: {count}</p>
+  //         <button onClick={subtractCount}>Subtract</button>
+  //         <button onClick={addCount}>Add</button>
+  //         <button onClick={changeColor}>Change Color</button>
+  //     </>
+  // );
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("Event Listener added");
+    
+    return() => {
+        window.removeEventListener("resize", handleResize);
+        console.log("Event Listener removed.");
+        
+    }
+  }, []);
+
+  useEffect(() => {
+    document.title = `${width} X ${height}`;
+  },[width, height])
+
+  return (
+    <>
+      <p>Window width: {width}px</p>
+      <p>Window height: {height}px</p>
+    </>
+  );
+}
+
+export default MyComponent;
